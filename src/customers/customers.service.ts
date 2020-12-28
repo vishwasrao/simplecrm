a4ad4e5aa1d1f31b0a8447c8abc4bcca5151b72f
Vishwasrao Salunkhe <vishwasrao.salunkhe@gmail.com>
2020-12-28 08:18:17 -0500

    Added method to create custoemr

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CustomersService {
-  private customers = [
-    {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@gmail.com',
-    },
-    {
-      firstName: 'Jane',
-      lastName: 'Doe',
-      email: 'jane.doe@gmail.com',
-    },
-  ];
+  private customers = [];
 
   getCustomers() {
     return this.customers;
+  }
+
+  createCustomer(firstName: string, lastName: string, email: string) {
+    const customerObject = {
+      id: uuidv4(),
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+    this.customers.push(customerObject);
+
+    return this.customers.push();
   }
 }
